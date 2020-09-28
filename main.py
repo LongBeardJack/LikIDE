b2a9d636ce3d6d69bc241d4b6c7adcfa06d6600e
BIPBIPGaminG <bipbipgaming.off@gmail.com>
2020-09-28 22:35:36 +0200

    update window system

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from window import Window
 
 window = Window("Titre ici", "1080x720")
+window.create_label(text="Test")
+window.create_label(text="Test2")
+window.add_all_label()
 
 if __name__ == "__main__":
     window.mainloop()
--- window.py
+++ window.py
@@ -4,11 +4,23 @@ import tkinter as tk
 class Window:
 
     root = None
+    labels = []
 
     def __init__(self, title="Title", geometry="300x300"):
         self.root = tk.Tk()
+
         self.root.geometry(geometry)
         self.root.title(title)
 
     def mainloop(self):
         self.root.mainloop()
+
+    def create_label(self, **kwargs):
+        self.labels.append(tk.Label(self.root, kwargs))
+
+    def add_label(self, index, **kwargs):
+        self.labels[index].pack(kwargs)
+
+    def add_all_label(self, **kwargs):
+        for label in self.labels:
+            label.pack(kwargs)
